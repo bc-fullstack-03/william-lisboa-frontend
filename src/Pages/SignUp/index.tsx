234cc944460071b04573a0c6cfad242de5693334
will763 <killzasper@gmail.com>
2023-05-23 20:09:44 -0300

    feat: implement handle register

--- src/Pages/SignUp/index.tsx
+++ src/Pages/SignUp/index.tsx
@@ -1,11 +1,25 @@
-import AuthForm from "../../components/AuthForm";
+import { useNavigate } from "react-router-dom";
+import AuthForm, { Auth } from "../../components/AuthForm";
+import api from "../../services/api";
 
 function SignUp(){
+    const navigate = useNavigate()
+
+    async function handleRegister(auth: Auth) {
+        try {
+            await api.post("/security/register", auth)
+            navigate("/")
+        } catch (err) {
+            alert("Erro na criação do usuário")
+        }
+    }
+
     return (
         <AuthForm 
             authFormTitle="Faça o cadastro e começe a usar!"
             routeName="/"
             submitFormButtonText="Cadastrar"
+            submitFormButtonAction={handleRegister}
         />
     )
 }
