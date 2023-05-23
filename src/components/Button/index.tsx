c06bd258dd724f9a7a5be0ba66019aa6230f3f68
will763 <killzasper@gmail.com>
2023-05-23 15:31:03 -0300

    feat: implement Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,26 @@
+import { Slot } from "@radix-ui/react-slot";
+import clsx from "clsx";
+import { ButtonHTMLAttributes, ReactNode } from "react";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    asChild?: boolean
+    children: ReactNode
+    className?: string
+}
+
+function Button({asChild, children,className, ...props}:ButtonProps){
+    const Comp = asChild ? Slot : "button";
+
+    return (
+        <Comp className={clsx(
+                "py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300",
+                className,
+                {...props}
+            )}
+        >
+            {children}
+        </Comp>
+    )
+}
+
+export default Button
