5e38c1effa0a44f501ac1112d19ae64556790ce3
will763 <killzasper@gmail.com>
2023-05-23 13:59:03 -0300

    feat: add Inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
