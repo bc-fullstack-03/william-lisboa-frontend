968a567ec3733268fdd4bff674576c9973330d41
will763 <killzasper@gmail.com>
2023-05-24 11:08:54 -0300

    feat: implement Home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,5 +1,32 @@
+import { getAuthHeader } from "../../services/auth"
+import api from "../../services/api";
+import { useEffect, useState } from "react";
+import Feed from "../../components/Feed";
+import MainScreen from "../../components/MainScreen";
+import { Post } from "../../Model/Post";
+
 function Home(){
-    return <h1>home</h1>
+    const [posts,setPosts] = useState<Post[]>([])
+    const authHeader = getAuthHeader();
+
+    useEffect(()=> {
+        async function getPosts() {
+            try {
+               const { data } = await api.get("/feed", authHeader);
+               setPosts(data)
+            } catch (error) {
+                alert("Error ao obter o Feed.")
+            }
+        }
+
+        getPosts();
+    }, [])
+
+    return (
+        <MainScreen>
+            <Feed posts={posts}/>
+        </MainScreen>
+    )
 }
 
 export default Home
