96b81132bc055037d5c5369bdf7e3a4aabafc45a
will763 <killzasper@gmail.com>
2023-05-24 17:44:58 -0300

    refactor: add new props

--- src/components/MainScreen/index.tsx
+++ src/components/MainScreen/index.tsx
@@ -1,14 +1,16 @@
+import { Post } from "../../Model/Post";
 import Menu from "../../components/Menu";
 import { ReactNode } from "react";
 
 interface MainScreenProps {
     children: ReactNode
+    postCreated?: (post:Post) => void
 }
 
 function MainScreen(props: MainScreenProps){
     return (
         <main className="flex w-screen h-screen">
-            <Menu />
+            <Menu postCreated={props.postCreated} />
             {props.children}
         </main>
     )
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,24 @@
+import * as Dialog from '@radix-ui/react-dialog';
 import { House, User, UsersThree } from '@phosphor-icons/react';
 import logo_menu from '../../assets/logo_menu.svg'
 import { MenuItem } from '../MenuItem';
 import Text from '../Text';
+import CreatePostButton from '../CreatePostButton';
+import CreatePostDialog from '../CreatePostDialog';
+import { Post } from '../../Model/Post';
+import { useState } from 'react';
 
+interface MenuProps {
+    postCreated?: (post:Post) => void
+}
 
-function Menu(){
+function Menu(props:MenuProps){
+    const [open, setOpen] = useState(false);
+
+    function postCreated(post:Post){
+        setOpen(false)
+        props.postCreated && props.postCreated(post);
+    }
     return (
         <div className="border-r basis-1/6 border-slate-400 ml-4 pt-4">
             <div className="flex items-center ml-4">
@@ -33,6 +47,13 @@ function Menu(){
                     <Text className='ml-4 font-extrabold'>Amigos</Text>
                 </MenuItem.Root>
             </ul>
+            <footer className='flex flex-col items-center'>
+                <Dialog.Root open={open} onOpenChange={setOpen}>
+                    <CreatePostButton />
+
+                    <CreatePostDialog postCreated={postCreated} />
+                </Dialog.Root>
+            </footer>
         </div>
     )
 }
