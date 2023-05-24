80c80e24a8663d6fa8e22eaddb3c70f472f20247
will763 <killzasper@gmail.com>
2023-05-24 08:18:35 -0300

    feat: implement routing to Profile Page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
 import Login from "./Pages/Login";
 import SignUp from "./Pages/SignUp";
 import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
   {
     path: "/home",
     element: <Home />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
   }
 ]);
 
