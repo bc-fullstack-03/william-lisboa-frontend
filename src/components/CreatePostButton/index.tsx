4684d1abd52608e45ce9d768ce25df183f006595
will763 <killzasper@gmail.com>
2023-05-24 17:42:45 -0300

    feat: add createPostButton component

---
+++ src/components/CreatePostButton/index.tsx
@@ -0,0 +1,11 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
+function CreatePostButton(){
+    return (
+       <Dialog.Trigger className='py-3 px-12 mt-6 bg-sky-500 transition-colors hover:bg-cyan-300 rounded-full font-semibold'>
+            Novo Post
+        </Dialog.Trigger>
+    )
+}
+
+export default CreatePostButton
