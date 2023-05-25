95d7629a3776589cfbf95eb5a0f933c468741571
will763 <killzasper@gmail.com>
2023-05-24 21:07:19 -0300

    feat: implement follow user

--- src/components/FriendsList/index.tsx
+++ src/components/FriendsList/index.tsx
@@ -3,7 +3,7 @@ import Heading from "../Heading";
 import Text from "../Text";
 import Button from "../Button";
 import { useEffect, useState } from "react";
-import { getAuthHeader } from "../../services/auth";
+import { getAuthHeader, getProfile } from "../../services/auth";
 import api from "../../services/api";
 
 interface Profile {
@@ -15,6 +15,7 @@ interface Profile {
 
 function FriendsList(){
     const [profiles,setProfiles] = useState<Profile[]>([]);
+    const myProfileId = getProfile();
     const authHeader = getAuthHeader();
 
     useEffect(()=>{
@@ -30,17 +31,53 @@ function FriendsList(){
         getProfiles();
     }, [])
 
+    async function handleFollow(profileId: string) {
+        try {
+          const profileIndex = profiles.findIndex((profile) => profile._id === profileId);
+    
+          if (profileIndex === -1) {
+            return;
+          }
+    
+          const profile = profiles[profileIndex];
+    
+          if (profile.followers.includes(myProfileId)) {
+            return;
+          }
+    
+          await api.post(`/profiles/${profileId}/follow`, null, authHeader);
+    
+          const updatedProfiles = [...profiles];
+          updatedProfiles[profileIndex] = {
+            ...profile,
+            followers: [...profile.followers, myProfileId],
+          };
+    
+          setProfiles(updatedProfiles);
+        } catch (err) {
+          alert("Erro ao tentar seguir o usuário");
+        }
+      }
+
     return (
-        <div>
-            <Heading className="ml-5 my-4">
-                <Text size="lg" className="font-extrabold">
+        <div className="basis-5/6 overflow-y-auto scroll-smooth">
+            <Heading  className="border-b border-slate-400 mt-4 my-5">
+                <Text size="lg" className="font-extrabold ml-5">
                     Amigos
                 </Text>
             </Heading>
-            {profiles && profiles.map((profile) => (
-                <div className="flex flex-col ml-5 w-full max-w-sm">
+            <ul>
+            {profiles && profiles.map((profile) => {
+                if (profile._id === myProfileId) {
+                    return null;
+                }
+                return (
+                    <li
+                      key={profile._id}
+                      className="flex flex-col ml-5 my-5  w-full max-w-sm"
+                    >
                     <div className="flex items-center">
-                        <UserCircle size={48} weight="thin" className="text-slate-50" />
+                        <UserCircle size={48} weight="thin" />
                         <Text className="ml-2 font-extrabold">{profile.name}</Text>
                     </div>
                     <div className="flex items-center ml-2">
@@ -55,9 +92,18 @@ function FriendsList(){
                                 `Seguindo ${profile.following.length} perfis`}
                         </Text>
                     </div>
-                    <Button className="my-2">Seguir</Button>
-                </div>
-            ))}
+                    <Button
+                       className={`my-4 ml-2 ${profile.followers.includes(myProfileId) ? "bg-gray-400 hover:bg-gray-400" : ""}`}
+                       onClick={() => handleFollow(profile._id)}
+                       disabled={profile.followers.includes(myProfileId)}
+                    >
+                       {profile.followers.includes(myProfileId) ? "Seguindo" : "Seguir"}
+                    </Button>
+                </li>
+                )               
+              }
+            )}
+          </ul>
         </div>
     )
 }
