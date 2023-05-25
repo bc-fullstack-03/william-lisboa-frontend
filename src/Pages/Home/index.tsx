916fdabfc32e2ae3041787cc7dfb0fecf2ad5c27
will763 <killzasper@gmail.com>
2023-05-24 21:04:21 -0300

    feat: implement like

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -4,9 +4,10 @@ import { useEffect, useState } from "react";
 import Feed from "../../components/Feed";
 import MainScreen from "../../components/MainScreen";
 import { Post } from "../../Model/Post";
+import { likePost, unLikePost } from "../../services/post";
 
 function Home(){
-    const user = localStorage.getItem("user") || "";
+    const profile = localStorage.getItem("profile") as string;
     const [posts,setPosts] = useState<Post[]>([])
     const authHeader = getAuthHeader();
 
@@ -23,20 +24,42 @@ function Home(){
         getPosts();
     }, [])
 
-    function postCreated(post:Post){
-        post = {
-            ...post,
-            profile: {
-                name: user
-            }
+    async function postCreated(post:Post){
+        try {
+            const { data } = await api.get(`/posts/${post._id}`, authHeader);
+            setPosts((posts) => [data, ...posts])
+        }catch(err){
+            alert("Error ao tentar obter post salvo.")
         }
-
-        setPosts((posts) => [post, ...posts])
     }
+
+    async function handleLike(postId:string){
+        const [post, ...rest] = posts.filter((post) => post._id === postId);
+
+        try {
+            if(post && !post.likes.includes(profile)){
+                const newPost = await likePost(post, profile);
+                changePosts(newPost);
+            } else{
+                const newPost = await unLikePost(post,profile)
+                changePosts(newPost)
+              }
+        } catch (error) {
+            alert("Error ao tentar realizar o like.")
+        }
+    }
+
+    function changePosts(newPost:Post){
+        setPosts((posts) => {             
+          const index = posts.indexOf(newPost)
+          posts[index] = newPost
+          return [...posts]
+        })
+      }
 
     return (
         <MainScreen postCreated={postCreated}>
-            <Feed posts={posts}/>
+            <Feed posts={posts} handleLike={handleLike}  />
         </MainScreen>
     )
 }
